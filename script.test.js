c33b2e4772a8debe3403a9cbdbe9ab580f840581
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-16 23:06:34 -0700

    Create test for 'calculator' object and its methods

--- script.test.js
+++ script.test.js
@@ -7,3 +7,10 @@ test("Capitalizes", () => {
 test("Reverses string", () => {
   expect(reverseString("P poT")).toBe("Top P");
 });
+
+test("Calculator", () => {
+  expect(calculator.add(3, 4.01)).toBeCloseTo(7.01);
+  expect(calculator.subtract(5, 3)).toBeCloseTo(2);
+  expect(calculator.divide(9, 3)).toBeCloseTo(3);
+  expect(calculator.multiply(9, 12)).toBeCloseTo(108);
+});
