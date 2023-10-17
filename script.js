8b7f33a22c1630e007e648e36dfc75445e95ed16
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-16 23:09:03 -0700

    Create 'add' method in 'calculator' object

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ function reverseString(string) {
   return string.split("").reverse().join("");
 }
 
-export { capitalize, reverseString };
+const calculator = {
+  add(num1, num2) {
+    return num1 + num2;
+  },
+};
+
+export { capitalize, reverseString, calculator };
