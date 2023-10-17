f499c323f95a0a207aad5211367243368d89d798
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-17 11:13:12 -0700

    Create test for 'analyzeArray' function

--- script.test.js
+++ script.test.js
@@ -24,3 +24,12 @@ test("Caesar Cipher", () => {
   );
   expect(caesarCipher("qrstuvwxyz", 1)).toBe("rstuvwxyza");
 });
+
+test("Analyze Array", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
