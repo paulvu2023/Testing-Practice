1f7601a53fd2795e2c4b2f34c005ff560d632679
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-16 22:59:17 -0700

    Create 'reverseString' function

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export { capitalize };
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+export { capitalize, reverseString };
