e38384eb4e4e61300e0602d904f8c0b6f83343f5
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-16 22:57:58 -0700

    Create test for 'reverseString' function

--- script.test.js
+++ script.test.js
@@ -1,5 +1,9 @@
-import { capitalize } from "./script";
+import { capitalize, reverseString } from "./script";
 
 test("Capitalizes", () => {
   expect(capitalize("paul")).toBe("Paul");
+});
+
+test("Capitalizes", () => {
+  expect(reverse("P poT")).toBe("Top P");
 });
