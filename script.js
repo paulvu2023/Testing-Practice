47cf6bdff064aef900a228304ce63ba04581e39a
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-16 23:10:33 -0700

    Add 'multiply' method in 'calculator' object

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ const calculator = {
   divide(num1, num2) {
     return num1 / num2;
   },
+
+  multiply(num1, num2) {
+    return num1 * num2;
+  },
 };
 
 export { capitalize, reverseString, calculator };
